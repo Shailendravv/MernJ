fda41802c315b22527d041b6cd554f0e612d2e67
Shailendra <shailendra.c@shyenatechyarn>
2022-12-20 20:09:37 +0530

    initial commit

---
+++ client/src/Pages/Dashboard.js
@@ -0,0 +1,20 @@
+import { response } from "express";
+import React, { useEffect } from "react";
+
+const Dashboard = () => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/v1");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
