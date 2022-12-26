636ee4a2532a788ed3779767b6e019092fd0268e
Shailendra <shailendra.c@shyenatechyarn>
2022-12-26 09:21:20 +0530

    server side work

--- client/src/Pages/dashboard/Profile.js
+++ client/src/Pages/dashboard/Profile.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useState } from "react";
+import { FormRow, Alert } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
 
 const Profile = () => {
+  const { user, showAlert, displayAlert, updateUser, isLoading } =
+    useAppContext();
+  const [name, setname] = useState(user?.name);
   return <div>Profile</div>;
 };
 
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -120,6 +120,10 @@ const AppProvider = ({ children }) => {
     dispatch({ type: LOGOUT_USER });
     removeUserFromLocalStorage();
   };
+  const updateUser = async (currentUser) => {
+    console.log("currentUser");
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -129,6 +133,7 @@ const AppProvider = ({ children }) => {
         loginUser,
         toggleSidebar,
         logoutUser,
+        updateUser,
       }}
     >
       {children}
