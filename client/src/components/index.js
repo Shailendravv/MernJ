2ad5bdc23bffd22c6b09b67ec376f39f307fa04b
Shailendra <shailendra.c@shyenatechyarn>
2022-12-28 08:18:47 +0530

    Add Job functionality implemented

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,16 @@ import Logo from "../components/Logo";
 import Alert from "./Alert";
 import BigSidebar from "./BigSidebar";
 import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
 import NavBar from "./NavBar";
 import SmallSidebar from "./SmallSidebar";
 
-export { Logo, FormRow, Alert, NavBar, BigSidebar, SmallSidebar };
+export {
+  Logo,
+  FormRow,
+  Alert,
+  NavBar,
+  BigSidebar,
+  SmallSidebar,
+  FormRowSelect,
+};
